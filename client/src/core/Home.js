34af61e389d1b9dbd21efea5fb4b0b8b34796a1e
Damir Cosic <damir.cosic67@gmail.com>
2020-10-11 22:45:04 +0200

    added max fails and fail timeout to nginx

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -38,7 +38,7 @@ export default function Home() {
   return (
     <Card className={classes.card}>
       <Typography variant="h6" className={classes.title}>
-        Home Page #@
+        Home Page v1
       </Typography>
       <CardMedia
         className={classes.media}
